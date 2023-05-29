396e3cf37565b0c602374899c392806b40034a35
saman <saman.noorzadeh@surgar-surgery.com>
2023-05-29 11:33:08 +0200

    Files added

---
+++ src/config.py
@@ -0,0 +1,12 @@
+ALL_CLASSES = ['background', 'treat', 'check']
+
+LABEL_COLORS_LIST = [
+    (255, 255, 255), # Background.
+    (128, 0, 0), # Treat
+    (0, 128, 0) # Check
+
+]
+
+VIS_LABEL_MAP = LABEL_COLORS_LIST
+
+ROOT_PATH = '/data/projects/DeeplabV3/input/incision/orig_data'
