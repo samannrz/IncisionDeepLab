4adeb03267c6b35fbbddb8b471f968d5232bf618
saman <saman.noorzadeh@surgar-surgery.com>
2023-09-14 16:10:24 +0200

    Files added

--- src/IOU_calculate.py
+++ src/IOU_calculate.py
@@ -6,7 +6,7 @@ from statistics import mean
 
 path_ref = '/data/projects/IncisionDeepLab/input/inference_data/'
 path_ref = '/data/DATA/incision/4/'
-path_inference = '/data/DATA/Incision_predictions/Batch8-9/consensus/'
+path_inference = '/data/DATA/Incision_predictions/Batch8-9/all_data/'
 
 
 # path_inference = '/data/DATA/incision/temp/1'
@@ -35,7 +35,7 @@ def calculate_confusion_matrix(mask_GT, mask_pred):
     return TP, FP, TN, FN
 
 
-epsilon = 1e-1
+epsilon = 1e-15
 
 ious_treat = []
 ious_check = []
@@ -46,9 +46,9 @@ sensitivity_check = []
 specificity_check = []
 f_treat = []
 f_check = []
-treat_row = [0,0,0]
-check_row = [0,0,0]
-bg_row = [0,0,0]
+treat_row = []
+check_row = []
+bg_row = []
 
 inf_masks = os.listdir(os.path.join(path_inference, 'mask', 'Treat'))
 for mask in inf_masks:
@@ -60,8 +60,7 @@ for mask in inf_masks:
     sensitivity_treat.append(TP_treat / ((TP_treat + FN_treat) + epsilon))
     specificity_treat.append(TN_treat / ((TN_treat + FP_treat) + epsilon))
     specificity_treat.append(TN_treat / ((TN_treat + FP_treat) + epsilon))
-    f_treat.append(((2 * TP_treat)+epsilon) / ((2 * TP_treat + FP_treat + FN_treat)+epsilon))
-
+    f_treat.append(((2 * TP_treat) + epsilon) / ((2 * TP_treat + FP_treat + FN_treat) + epsilon))
 
     mask_ref_check = cv2.imread(os.path.join(path_ref, 'mask', 'Check', mask))
     mask_inf_check = cv2.imread(os.path.join(path_inference, 'mask', 'Check', mask))
@@ -72,10 +71,27 @@ for mask in inf_masks:
     f_check.append(2 * TP_check / (2 * TP_check + FP_check + FN_check))
 
     mask_ref_treat = mask_ref_treat != 0
+    mask_ref_check = mask_ref_check != 0
 
-    treat_row = treat_row + [TP_treat+epsilon,np.sum(np.logical_and(mask_ref_treat,mask_inf_check))+epsilon, np.sum(np.logical_and(mask_ref_treat,~np.logical_or(mask_inf_check,mask_inf_treat)))+epsilon] / np.sum(mask_ref_treat)+epsilon
-    print(treat_row)
-    print(sum(treat_row))
+    if np.sum(mask_ref_treat) != 0:
+        treat_row.append([TP_treat,
+                          np.sum(np.logical_and(mask_ref_treat, mask_inf_check)),
+                          np.sum(np.logical_and(mask_ref_treat, ~np.logical_or(mask_inf_check, mask_inf_treat)))] / (
+                             np.sum(mask_ref_treat)))
+    if np.sum(mask_ref_check) != 0:
+        check_row.append([np.sum(np.logical_and(mask_ref_check, mask_inf_treat)),
+                          TP_check,
+                          np.sum(np.logical_and(mask_ref_check, ~np.logical_or(mask_inf_check, mask_inf_treat)))] / (
+                             np.sum(mask_ref_check)))
+
+    mask_ref_bg = np.logical_and(~ mask_ref_check, ~ mask_ref_treat)
+    mask_inf_bg = np.logical_and(~ mask_inf_check, ~ mask_inf_treat)
+    bg_row.append([np.sum(np.logical_and(mask_ref_bg, mask_inf_treat)),
+                   np.sum(np.logical_and(mask_ref_bg, mask_inf_check)),
+                   np.sum(np.logical_and(mask_ref_bg, mask_inf_bg))] / np.sum(mask_ref_bg))
+    # print((bg_row))
+    # print(treat_row)
+    # print(check_row)
 
 print(len(mask_list))
 print(mask_list)
@@ -89,5 +105,28 @@ print('Check sensitivity = ', mean(sensitivity_check))
 print('Check specificity = ', mean(specificity_check))
 print('Treat F-Score = ', mean(f_treat))
 print('Check F-Score = ', mean(f_check))
-print(treat_row)
+conf_matrix = [list(np.mean(treat_row, 0)), list(np.mean(check_row, 0)), list(np.mean(bg_row, 0))]
+print('conf_matrix_treat : ', np.mean(treat_row, 0))
+print('conf_matrix_check : ', np.mean(check_row, 0))
+print('conf_matrix_backg : ', np.mean(bg_row, 0))
 
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.imshow(conf_matrix,cmap='Blues')
+for i in range(3):
+    for j in range(3):
+        c = conf_matrix[j][i]
+        plt.text(i, j, str(round(c,3)), va='center', ha='center')
+plt.text(0, -.6, 'treat', va='center', ha='center')
+plt.text(1, -.6, 'check', va='center', ha='center')
+plt.text(2, -.6, 'background', va='center', ha='center')
+plt.text(-.6, 0, 'treat', va='center', ha='center',rotation = 'vertical')
+plt.text(-.6, 1, 'check', va='center', ha='center',rotation = 'vertical')
+plt.text(-.6, 2, 'background', va='center', ha='center',rotation = 'vertical')
+plt.text(1, -.8, 'Prediction', va='center', ha='center')
+plt.text(-.8, 1, 'Ground Truth', va='center', ha='center', rotation = 'vertical')
+
+plt.xticks([])
+plt.yticks([])
+plt.savefig('all-data.png')
