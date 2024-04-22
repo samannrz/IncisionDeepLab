d230559de31ac048f8c77f071bdcaad87033ff94
saman <saman.noorzadeh@surgar-surgery.com>
2024-04-22 14:23:09 +0200

    submitBash added

--- src/submitJob.sh
+++ src/submitJob.sh
@@ -3,11 +3,11 @@
 ##Resource Request
 
 #SBATCH --job-name incisionDeeplab
-#SBATCH --output=mnistResult.stdout
-#SBATCH --error=mnistResult.sdterr
-#SBATCH --ntasks=1  ## number of tasks (analyses) to run
+#SBATCH --output=stdout
+#SBATCH --error=sdterr
+#SBATCH --ntasks=2  ## number of tasks (analyses) to run
 #SBATCH --gpus-per-task=2 # number of gpus per task
-#SBATCH --mem-per-gpu=1000M # Memory allocated per gpu
+#SBATCH --mem-per-gpu=7000M # Memory allocated per gpu
 #SBATCH --time=0-00:10:00  ## time for analysis (day-hour:min:sec)
 
 ## Run the script
