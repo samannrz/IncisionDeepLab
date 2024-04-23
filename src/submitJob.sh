b0c74286585fcd6bf7e68aded4b99698bb535593
saman <saman.noorzadeh@surgar-surgery.com>
2024-04-23 14:13:29 +0200

    submitBash added

--- src/submitJob.sh
+++ src/submitJob.sh
@@ -7,8 +7,8 @@
 #SBATCH --error=sdterr
 #SBATCH --ntasks=1  ## number of tasks (analyses) to run
 #SBATCH --gpus-per-task=1 # number of gpus per task
-#SBATCH --mem-per-gpu=20000M # Memory allocated per gpu
-#SBATCH --time=0-02:00:00  ## time for analysis (day-hour:min:sec)
+#SBATCH --mem-per-gpu=24000M # Memory allocated per gpu
+#SBATCH --time=0-03:00:00  ## time for analysis (day-hour:min:sec)
 
 ## Run the script
 srun python train.py --epochs 300 --batch 20 --scheduler
